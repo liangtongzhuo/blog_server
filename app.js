7ab92b8a8bdfa749e1d3202e3c14780920d6732e
liangya363138911@126.com <daoj8hao>
2017-11-09 19:23:38 +0800

    x

--- app.js
+++ app.js
@@ -29,52 +29,10 @@ app.use(views(path.join(__dirname, 'views')));
 // 设置静态资源目录
 app.use(statics(path.join(__dirname, 'public')));
 
-app.use(wechat(config).middleware(async (message, ctx) => {
-  // 微信输入信息就是这个 message
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    return 'hehe';
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    return {
-      content: 'text object',
-      type: 'text'
-    };
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    return {
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3"
-      }
-    };
-  } else if (message.FromUserName === 'kf') {
-    // 转发到客服接口
-    return {
-      type: "customerService",
-      kfAccount: "test1@test"
-    };
-  } else {
-    // 回复高富帅(图文回复)
-    return [
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ];
-  }
-}));
-
-
 const router = new Router();
 app.use(router.routes());
 
-// 加载云引擎中间件
+// // 加载云引擎中间件
 app.use(AV.koa());
 
 app.use(bodyParser());
@@ -87,6 +45,48 @@ router.get('/', async function (ctx) {
 // 可以将一类的路由单独保存在一个文件中
 app.use(require('./routes/todos').routes());
 
+app.use(wechat(config).middleware(async (message, ctx) => {
+ // 微信输入信息就是这个 message
+ if (message.FromUserName === 'diaosi') {
+  // 回复屌丝(普通回复)
+  return 'hehe';
+} else if (message.FromUserName === 'text') {
+  //你也可以这样回复text类型的信息
+  return {
+    content: 'text object',
+    type: 'text'
+  };
+} else if (message.FromUserName === 'hehe') {
+  // 回复一段音乐
+  return {
+    type: "music",
+    content: {
+      title: "来段音乐吧",
+      description: "一无所有",
+      musicUrl: "http://mp3.com/xx.mp3",
+      hqMusicUrl: "http://mp3.com/xx.mp3"
+    }
+  };
+} else if (message.FromUserName === 'kf') {
+  // 转发到客服接口
+  return {
+    type: "customerService",
+    kfAccount: "test1@test"
+  };
+} else {
+  // 回复高富帅(图文回复)
+  return [
+    {
+      title: '你来我家接我吧',
+      description: '这是女神与高富帅之间的对话',
+      picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+      url: 'http://nodeapi.cloudfoundry.com/'
+    }
+  ];
+}
+}));
+
+
 module.exports = app;
 
 
@@ -94,49 +94,3 @@ module.exports = app;
 
 
 
-
-// 下面用于验证微信 token
-/*
-var http = require("http");
-var url = require("url");
-var crypto = require("crypto");
-
-function sha1(str){
-  var md5sum = crypto.createHash("sha1");
-  md5sum.update(str);
-  str = md5sum.digest("hex");
-  return str;
-}
-
-function validateToken(req,res){
-  var query = url.parse(req.url,true).query;
-  var signature = query.signature;
-  var echostr = query.echostr;
-  var timestamp = query['timestamp'];
-  var nonce = query.nonce;
-  var oriArray = new Array();
-  oriArray[0] = nonce;
-  oriArray[1] = timestamp;
-  oriArray[2] = "liangtongzhuo";//这里是你在微信开发者中心页面里填的 token，而不是****
-  oriArray.sort();
-  var original = oriArray.join('');
-  console.log("Original str : " + original);
-  console.log("Signature : " + signature );
-  var scyptoString = sha1(original);
-  if(signature == scyptoString){
-    res.end(echostr);
-    console.log("Confirm and send echo back");
-  }else {
-    res.end("false");
-    console.log("Failed!");
-  }
-}
-
-
-var webSvr = http.createServer(validateToken);
-webSvr.listen(function(){
-  console.log("Start validate");
-});
-
-module.exports = webSvr;
-*/
