e85b845c0c0d6b7a0d69973ceabb6ebefe3c1450
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-11-16 08:55:48 +0800

    修

--- controller/reply_controller.js
+++ controller/reply_controller.js
@@ -6,13 +6,13 @@ function reply(message, ctx) {
     
     //关注
     if (message.Event == 'subscribe') {
-        return '你关注了梁同桌\n博客: liangtongzhuo.com\n知乎、bilibili微博、github，都为梁同桌\n这个公众号会有一些有意思的事！😳😳😳';
+        return '你关注了梁同桌\n博客: liangtongzhuo.com\n知乎、bilibili微博、github，都为梁同桌\n这个公众号会搞一些有意思的事！😳😳😳';
     }
     // 文本
     if (message.MsgType == 'text') {
         if (message.Content == '1') {
             const arr = ['四川饭庄', '兰州拉面', '饺子馆'];
-            let i = parseInt(Math.random() * 100) % 3;
+            let i = parseInt(Math.random() * 90) % 3;
             return arr[i];
         }
 
