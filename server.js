0a1e3110be87a0a13a5de4c5d0b74939432208ae
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-20 13:06:24 +0800

    定时器

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { setInterval } from 'timers';
+
 var AV = require('leanengine')
 var request = require('request')
 
@@ -28,24 +30,30 @@ app.listen(PORT, function (err) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack)
   });
 
-  // 获取微信 AccessToken
+  // 定时获取微信 AccessToken
+  setInterval(() => {
+    timeUpData()
+  }, 1000 * 60 * 60 * 1.5)
+    timeUpData()
+})
+
+function timeUpData() {
   updateAccessToken()
     .then(data => {
       console.log('AccessToken', data)
-      global.access_token =  data.access_token //复制为全局
+      global.access_token = data.access_token //复制为全局
     })
     .catch(err => {
       console.log('验证 AccessToken 错误：', err)
     })
-})
-
+}
 
 // 票据更新
 function updateAccessToken() {
   var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + process.env.wechat_appid + '&secret=' + process.env.wechat_appSecret
   return new Promise(function (resolve, reject) {
     // 发起一个get请求
-    request( url, (err, response) => {
+    request(url, (err, response) => {
       if (err) return reject(err)
       resolve(response.body)
     })
