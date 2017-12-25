e4badc71abc7a22929c0685f02f07f1350db7a36
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-25 10:35:20 +0800

    AccessToke

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var AV = require('leanengine')
-var request = require('request')
 
 // 环境变量中获取, 并初始化.
 AV.init({
@@ -27,33 +26,5 @@ app.listen(PORT, function (err) {
   process.on('unhandledRejection', function (reason, p) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack)
   });
-
-  // 定时获取微信 AccessToken
-  setInterval(() => {
-    timeUpData()
-  }, 1000 * 60 * 60 * 1.5)
-  timeUpData()
 })
 
-function timeUpData() {
-  updateAccessToken()
-    .then(data => {
-      console.log('AccessToken', data)
-      global.access_token = data.access_token //复制为全局
-    })
-    .catch(err => {
-      console.log('验证 AccessToken 错误：', err)
-    })
-}
-
-// 票据更新
-function updateAccessToken() {
-  var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + process.env.wechat_appid + '&secret=' + process.env.wechat_appSecret
-  return new Promise(function (resolve, reject) {
-    // 发起一个get请求
-    request(url, (err, response) => {
-      if (err) return reject(err)
-      resolve(response.body)
-    })
-  })
-}
