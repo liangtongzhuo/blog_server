6baf7150ba4ce96485196e37a25fd70f18a42aea
ltz <liangtongzhuo@gmail.com>
2018-10-15 00:08:26 +0800

    add query db reply

--- controller/replyController.js
+++ controller/replyController.js
@@ -1,46 +1,32 @@
 'use strict';
 const rp = require('request-promise');
+const AV = require('leanengine');
+const WeChatReply = AV.Object.extend('WeChatReply');
+let weChatReplys;
+(async function() {
+  const query = new AV.Query(WeChatReply);
+  try {
+    weChatReplys = await query.find();
+  } catch (e) {
+    console.log('query weChatReplys', e);
+  }
+})();
 
 const reply = async (message, ctx) => {
   console.log('-----', message, ctx.request);
   //关注
   if (message.Event == 'subscribe') {
-    return '你关注了梁同桌公众号\n博客: liangtongzhuo.com\n知乎、bilibili、微博、github，id 都为「梁同桌」\n这个公众号会搞一些有意思的事，也可能万年不更新！我也帮助梁同桌选择午饭，也可你陪你聊天啦。';
+    return '哇塞！你关注了梁同桌公众号，他的其它社交媒体有\n博客: liangtongzhuo.com\n知乎、bilibili、微博、GitHub，ID 都为「梁同桌」\n这个公众号会搞一些有意思的事，也可能万年不更新！我也帮助梁同桌选择午饭，回复「吃饭」，也可你陪你聊天啦，回复 [任意文字]';
   }
   // 文本
   if (message.MsgType == 'text') {
-    if (message.Content == '吃饭' || message.Content == '换一家') {
-      const arr = [
-        '西边盖浇饭',
-        '西边茄汁面',
-        '北边盖浇饭',
-        '东边麻辣豆腐',
-        '东边臊子面',
-        '东边热干面',
-        '东边胡辣汤',
-        '东边清真饭店',
-        '北边会面',
-      ];
-      const random = parseInt(Math.random() * arr.length);
-      return arr[random] + '，不想吃回复“换一家” :)';
-    }
-
-    if (message.Content == '喜欢你' || message.Content == '我喜欢你') {
-      return '我不会告诉梁同桌的，但是我会记到数据库离 :)，梁同桌那傻子是不会看的 :(';
-    }
-
-    if (message.Content == '不喜欢你' || message.Content == '我不喜欢你') {
-      return '哼！讨厌 :(';
-    }
-
-    if (message.Content == '你喜欢谁' || message.Content == '你喜欢谁？') {
-      return '讨厌，正在和我聊天的人。 害羞.jpg';
-    }
+    const replyMessage = getMessage(message.Content);
+    if (replyMessage) return replyMessage;
 
     // 请求智能返回语句
     return await postRequest(message.FromUserName, message.Content);
   }
-  return '不听不听\n回复 “吃饭”，选择餐馆 :)';
+  return '不听不听\n回复「吃饭」，选择餐馆 :)';
 };
 
 // 发送post请求
@@ -70,6 +56,27 @@ async function postRequest(userId, content) {
   } catch (error) {
     return '梁同桌今天可能累，去睡觉觉了';
   }
+}
+
+// 根据信息信息返回回复文字信息
+function getMessage(message) {
+  weChatReplys.forEach(weChatReply => {
+    weChatReply
+      .get('key')
+      .split(',')
+      .forEach(keySub => {
+        // 匹配到了则回复
+        if (keySub.indexOf(message) !== -1) {
+          const values = weChatReply.get('value').split(',');
+          if (values.length === 1) {
+            return values[0];
+          } else {
+            const random = parseInt(Math.random() * values.length);
+            return values[random];
+          }
+        }
+      });
+  });
 }
 
 module.exports.reply = reply;
