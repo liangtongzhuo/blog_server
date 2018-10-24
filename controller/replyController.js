11eaf1a05b14b021a3d25c79bdfe914ae546ebb5
ltz <liangtongzhuo@gmail.com>
2018-10-24 18:25:16 +0800

    update info

--- controller/replyController.js
+++ controller/replyController.js
@@ -16,7 +16,7 @@ const reply = async (message, ctx) => {
   console.log('-----', message, ctx.request);
   //关注
   if (message.Event == 'subscribe') {
-    return '哇塞！你关注了梁同桌公众号，他的其它社交媒体有\n博客: liangtongzhuo.com\n知乎、bilibili、微博、GitHub，ID 都为「梁同桌」\n这个公众号会搞一些有意思的事，也可能万年不更新！\n\n你也帮助梁同桌选择午饭，回复「吃饭」。\n梁同桌也可你陪你聊天，回复「任意文字」';
+    return '哇塞！你关注了梁同桌公众号，他的其它社交媒体有:\n个人博客:http://www.liangtongzhuo.com\n\nbilibili 同人音声:https://space.bilibili.com/3591966\n\n微博私人账单:https://www.weibo.com/tongrenyinsheng\n\nGitHub 技术开源:https://github.com/liangtongzhuo\n\n知乎价值观:https://www.zhihu.com/people/liangtongzhuo\n\n多人在线打飞机游戏:http://planes.liangtongzhuo.com\n\n这个公众号会搞一些有意思的事，也可能万年不更新！\n\n你也帮助梁同桌选择午饭，回复「吃饭」。\n梁同桌也可你陪你聊天，回复「任意文字」';
   }
   // 文本
   if (message.MsgType == 'text') {
